e1ba7feda63b15e6261c7d0e004c91003ab197ab
letasbulhas <let.bulhoes@gmail.com>
2023-08-29 20:53:36 -0300

    primeiro commit

---
+++ src/main/java/DesafioNextSeguros2023/DesafioNextSeguros2023/DesafioNextSeguros2023Application.java
@@ -0,0 +1,13 @@
+package DesafioNextSeguros2023.DesafioNextSeguros2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioNextSeguros2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioNextSeguros2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/DesafioNextSeguros2023/DesafioNextSeguros2023/DesafioNextSeguros2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package DesafioNextSeguros2023.DesafioNextSeguros2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioNextSeguros2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
